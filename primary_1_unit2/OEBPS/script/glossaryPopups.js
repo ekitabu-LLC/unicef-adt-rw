426f05981e4123e7831160ff063fc522eece9674
dobbz <dobbz.mkolwe@ekitabu.com>
2020-07-28 22:59:26 +0300

    incorporated changes from all branches

---
+++ primary_1_unit2/OEBPS/script/glossaryPopups.js
@@ -0,0 +1,113 @@
+function deskFun() {
+		var popup = document.getElementById("desk");
+  		popup.classList.toggle("show");
+	
+}
+
+function thisFun() {
+		var popup = document.getElementById("this");
+  		popup.classList.toggle("show");
+	
+}
+
+function windowFun() {
+		var popup = document.getElementById("window");
+  		popup.classList.toggle("show");
+	
+}
+
+function thatFun() {
+		var popup = document.getElementById("that");
+  		popup.classList.toggle("show");
+	
+}
+
+function cupboardFun() {
+		var popup = document.getElementById("cupboard");
+  		popup.classList.toggle("show");
+	
+}
+
+function penFun() {
+		var popup = document.getElementById("pen");
+  		popup.classList.toggle("show");
+	
+}
+
+function colorFun() {
+		var popup = document.getElementById("color");
+  		popup.classList.toggle("show");
+	
+}
+
+function yellowFun() {
+		var popup = document.getElementById("yellow");
+  		popup.classList.toggle("show");
+	
+}
+
+function blueFun() {
+		var popup = document.getElementById("blue");
+  		popup.classList.toggle("show");
+	
+}
+
+function theseFun() {
+		var popup = document.getElementById("these");
+  		popup.classList.toggle("show");
+	
+}
+
+function whiteFun() {
+		var popup = document.getElementById("white");
+  		popup.classList.toggle("show");
+	
+}
+
+function havegotFun() {
+		var popup = document.getElementById("havegot");
+  		popup.classList.toggle("show");
+	
+}
+
+function bagFun() {
+		var popup = document.getElementById("bag");
+  		popup.classList.toggle("show");
+	
+}
+
+function greenFun() {
+		var popup = document.getElementById("green");
+  		popup.classList.toggle("show");
+	
+}
+
+function violetFun() {
+		var popup = document.getElementById("violet");
+  		popup.classList.toggle("show");
+	
+}
+
+function orangeFun() {
+		var popup = document.getElementById("orange");
+  		popup.classList.toggle("show");
+	
+}
+
+function redFun() {
+		var popup = document.getElementById("red");
+  		popup.classList.toggle("show");
+	
+}
+
+function indigoFun() {
+		var popup = document.getElementById("indigo");
+  		popup.classList.toggle("show");
+	
+}
+
+function sheFun() {
+		var popup = document.getElementById("she");
+  		popup.classList.toggle("show");
+	
+}
