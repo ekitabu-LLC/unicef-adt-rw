3ad6abe927d68b1612e26d5eea9c67921258b6bb
Muhannad Darnasser <mnaser@iconnecths.com>
2020-11-23 13:14:36 +0200

    adding video changes

--- 03_Entire_Book/epub_template/OEBPS/Misc/book.js
+++ 03_Entire_Book/epub_template/OEBPS/Misc/book.js
@@ -2,11 +2,18 @@ $(document).ready(function(){
     var videoContainer = $("#video-player");
     var videoSrc = $("#video-player video source");
     var videoElement =$("#video-player video");
+    
     $(".show-video").click(function(){
         videoSrc.attr("src",$(this).attr("video"))
         videoElement.attr("poster",$(this).attr("poster"))
         videoContainer.show();
     })
+
+    $("#videoclose").click(function(event){
+        event.stopPropagation();
+        videoContainer.hide();
+
+    });
 
     videoContainer.click(function(){
         videoContainer.hide();
